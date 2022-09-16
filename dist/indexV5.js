4c9117f3c72e8404fbc71b3f2655164294c110d1
mohamedtamer0 <mt4452242@gmail.com>
2022-09-16 21:35:51 +0200

    update

---
+++ dist/indexV5.js
@@ -0,0 +1,8 @@
+"use strict";
+let theName = "Tamer";
+let theAge = 40;
+let hire = true;
+let all = "Mohamed Tamer";
+theName = "Tamer";
+all = 100;
+//# sourceMappingURL=indexV5.js.map
---
+++ src/indexV5.ts
@@ -0,0 +1,19 @@
+// type Annotations || Signature
+
+let theName:string = "Tamer";
+let theAge: number = 40;
+let hire:boolean = true;
+
+let all:any = "Mohamed Tamer"
+
+theName = "Tamer";
+all = 100;
+
+//////////////
+
+function add(n1:number,n2:number) {
+    return n1 + n2;
+}
+
+console.log(add(10, 20));
+console.log(typeof add(10, 20));
