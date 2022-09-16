22e60a6e76750872972c9905688bb33628262590
mohamedtamer0 <mt4452242@gmail.com>
2022-09-16 20:58:39 +0200

    update

---
+++ dist/index2.js
@@ -0,0 +1,8 @@
+"use strict";
+let age = 40;
+if (age > 30) {
+    console.log("Good");
+}
+else {
+}
+//# sourceMappingURL=index2.js.map
---
+++ src/index2.ts
@@ -0,0 +1,11 @@
+// let num = 10;
+// num = "Tamer";
+// console.log(num)
+
+let age = 40 ;
+
+if(age > 30) {
+    console.log("Good");
+} else {
+    // console.log(age.repat(3));
+}
