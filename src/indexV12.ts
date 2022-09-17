97bebe690513bd424dcd6c45150df78c1f02b1b3
mohamedtamer0 <mt4452242@gmail.com>
2022-09-17 10:32:59 +0200

    update

---
+++ src/indexV12.ts
@@ -0,0 +1,10 @@
+// Data Types - Type Alias
+
+type st = string;
+let Name1:st = "Mohamed";
+Name1 = "Tamer";
+
+type standnum = string | number;
+let all2 : standnum = 10;
+all = 100;
+all = "Ali";
