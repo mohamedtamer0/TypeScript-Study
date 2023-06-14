aa7a35300f6b4cec836a78e945fe61e23d307f4a
mohamed.tamer <mt4452242@gmail.com>
2023-06-14 08:54:57 +0300

    update

---
+++ dist/Enums.js
@@ -0,0 +1,26 @@
+"use strict";
+const KID = 15;
+const EASY = 9;
+const MEDIUM = 6;
+const HARD = 3;
+function getHardSeconds() {
+    return 3;
+}
+var kids;
+(function (kids) {
+    kids[kids["Five"] = 25] = "Five";
+    kids[kids["seven"] = 20] = "seven";
+    kids[kids["Ten"] = 15] = "Ten";
+})(kids || (kids = {}));
+var Level;
+(function (Level) {
+    Level[Level["Kid"] = 15] = "Kid";
+    Level[Level["Easy"] = 9] = "Easy";
+    Level[Level["Medium"] = 6] = "Medium";
+    Level[Level["Hard"] = getHardSeconds()] = "Hard";
+})(Level || (Level = {}));
+let lvl = "Easy";
+if (lvl === "Easy") {
+    console.log(`The Level ${lvl} And Of Second is ${Level.Hard}`);
+}
+//# sourceMappingURL=Enums.js.map
--- dist/InterfaceDeclaration.js
+++ dist/InterfaceDeclaration.js
@@ -1,25 +1,15 @@
-// Interface Declaration
-
-
-interface User {
-    id?: number,
-    readonly username: string,
-    country: string
-}
-
-let user: User = {
+"use strict";
+let user = {
     id: 100,
     username: "Tamer",
     country: "Egypt"
-}
-
+};
 user.country = "Syria";
 console.log(user);
-
-function getData(data: User) {
+function getData(data) {
     console.log(`Id is ${data.id}`);
     console.log(`Username is ${data.username}`);
     console.log(`Country is ${data.country}`);
 }
-
 getData({ id: 200, username: "Mohamed", country: "KSA" });
+//# sourceMappingURL=InterfaceDeclaration.js.map
