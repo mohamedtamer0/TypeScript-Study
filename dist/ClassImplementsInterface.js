493e2e126e9c0d67c68710394b2a4c99a88abd4d
mohamed.tamer <mt4452242@gmail.com>
2023-06-16 18:58:11 +0300

    update

--- dist/ClassImplementsInterface.js
+++ dist/ClassImplementsInterface.js
@@ -1,10 +1,20 @@
 "use strict";
 class UserImp {
-    constructor(theme) {
+    constructor(username, theme, font) {
+        this.username = username;
         this.theme = theme;
+        this.font = font;
     }
     save() {
         console.log('Saved');
     }
+    update() {
+        console.log('Updated');
+    }
 }
+let UserOne = new UserImp("Tamer", true, "Open Sans");
+console.log(UserOne.username);
+console.log(UserOne.font);
+UserOne.save();
+UserOne.update();
 //# sourceMappingURL=ClassImplementsInterface.js.map
--- src/ClassImplementsInterface.ts
+++ src/ClassImplementsInterface.ts
@@ -1,12 +1,24 @@
 interface SettingsImp {
     theme: boolean;
-    font?: string;
+    font: string;
     save(): void;
 }
 
 class UserImp implements SettingsImp {
-    constructor(public theme: boolean) { }
+    constructor(public username: string, public theme: boolean, public font: string) { }
     save(): void {
         console.log('Saved');
     }
+
+    update(): void {
+        console.log('Updated');
+    }
 }
+
+let UserOne = new UserImp("Tamer", true, "Open Sans");
+
+console.log(UserOne.username);
+console.log(UserOne.font);
+
+UserOne.save();
+UserOne.update();
